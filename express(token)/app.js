bbfc12e0096d63720d9836c124df7ae97bce78b2
Zhangyao719 <1258858403@qq.com>
2021-11-17 23:57:37 +0800

    feat(token): router

---
+++ express(token)/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const apiRouter = require('./router/apiRouter')
+const v1Router = require('./router/v1Router')
+
+const app = express()
+
+// 给路由添加前缀 (命名空间, 配置多组路由对象)
+app.use('/api', apiRouter)
+app.use('/v1', v1Router)
+
+app.listen(8001, () => {
+  console.log('服务器启动成功, http://localhost:8001')
+})
---
+++ express(token)/router/v1Router.js
@@ -0,0 +1,5 @@
+const express = require('express')
+
+const v1Router = express.Router()
+
+module.exports = v1Router
