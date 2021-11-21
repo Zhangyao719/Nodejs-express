566042c73dfa406f43cfdb51b40dcb7f1e4fc0d3
Zhangyao719 <1258858403@qq.com>
2021-11-21 19:35:28 +0800

    feat: cors

--- express(token)/app.js
+++ express(token)/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const apiRouter = require('./router/apiRouter')
 const v1Router = require('./router/v1Router')
+const cors = require('cors')
 
 const app = express()
 
 // 给路由添加前缀 (命名空间, 配置多组路由对象)
+app.use(cors())
 app.use('/api', apiRouter)
 app.use('/v1', v1Router)
 
--- express(token)/router/v1Router.js
+++ express(token)/router/v1Router.js
@@ -2,4 +2,23 @@ const express = require('express')
 
 const v1Router = express.Router()
 
+v1Router.get('/getAll', (req, res) => {
+  res.send({
+    status: 200,
+    message: 'success',
+    list: [
+      { name: 'zs', age: 18 },
+      { name: 'ls', age: 19 },
+    ]
+  })
+})
+
+v1Router.post('/add', (req, res) => {
+  console.log(req);
+  res.send({
+    status: 200,
+    message: 'add success',
+  })
+})
+
 module.exports = v1Router
